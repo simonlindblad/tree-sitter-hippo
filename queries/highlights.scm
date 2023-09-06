9daf514a3e2b19c531a4178e63bf6b906b4b0d3a
Simon Lindblad <simon.lindblad93@gmail.com>
2023-09-05 21:32:35 -0400

    highlight variables

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,37 +1,11 @@
 [
-  (function) @function
-
-  (comment) @comment
-
   ; Function Calls
   (function_call
-    function: (identifier) @function
-    (argument
-        (identifier) @variable))
-
-  (function_call
     function: (identifier) @function)
-
-  ; Assignments
-  (assignment
-    left: (identifier) @variable
-    "=" @punctuation.separator.key-value)
 
   ; Data Definitions
   (data_definition
     (identifier) @type)
-
-  (data_definition_parameter
-    (identifier) @variable.parameter)
-    
-
-  ; Column Identifiers
-  (column_identifier
-    "@" @punctuation.special
-    (identifier) @variable)
-
-  (alias
-    (identifier) @variable)
 
   ; Binary Expressions
   ((binary_expression operator: (or_operator)) @operator)
@@ -43,6 +17,10 @@
   ; General Identifiers and Literals
   (num_literal) @number
   (string_literal) @string
+
+  (identifier) @variable
+  (comment) @comment
+
 
   "{" @punctuation.bracket
   "}" @punctuation.bracket
